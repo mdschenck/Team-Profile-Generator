4eca6e66be0ee78b6e8d5fe67c2bcdd30bd1a668
MSch33 <99237120+mdschenck@users.noreply.github.com>
2022-05-07 15:18:30 -0400

    adding constructor function skeleton

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,10 +1,21 @@
 const Employee = require("../lib/Employee");
 
+test("Can instantiate Employee instance", () => {
+    const e new Employee();
+    expect(typeof(e)).toBe("object");
+});
+
 test("Can set ID via constructor argument", () => {
   const testValue = 100;
   const e = new Employee("foo", testValue);
   expect(e.id).toBe(testValue);
 });
+
+test("Can set Name via constructor argument", () => {
+    const testValue = "Jimmy";
+    const e = new Employee("foo", 1, testValue);
+    expect(e.name).toBe(testValue);
+  });
 
 test("Can set Email via constructor argument", () => {
   const testValue = "test@test.com";
